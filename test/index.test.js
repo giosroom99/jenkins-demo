8ec997bfa5b2fa89a27ec661afdafab626342066
colmak <69098858+colmak@users.noreply.github.com>
2024-09-18 11:34:19 -0400

    update tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,13 +1,23 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const app = require('../src/index'); // Assuming you export the app in src/index.js
+const app = require('../src/index'); // Import the app instance
 
 chai.use(chaiHttp);
 const expect = chai.expect;
 
 describe('GET /', () => {
+  let server;
+
+  before((done) => {
+    server = app.listen(3001, done); // Start the server on a different port for testing
+  });
+
+  after((done) => {
+    server.close(done); // Close the server after tests
+  });
+
   it('should return Hello World', (done) => {
-    chai.request(app)
+    chai.request(server)
       .get('/')
       .end((err, res) => {
         expect(res).to.have.status(200);
@@ -17,7 +27,7 @@ describe('GET /', () => {
   });
 
   it('should fail by returning Goodbye World', (done) => {
-    chai.request(app)
+    chai.request(server)
       .get('/')
       .end((err, res) => {
         expect(res).to.have.status(200);
